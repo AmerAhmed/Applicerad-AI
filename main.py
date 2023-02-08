3263ecc3e0c5399907da05a98908c7cd37ff3c50
Amer Ahmed <anderssonamer@gmail.com>
2023-02-08 14:54:58 +0100

    Updated projest

--- main.py
+++ main.py
@@ -4,23 +4,20 @@ import numpy as np
 from tensorflow.python.keras.models import load_model
 from fastapi import FastAPI
 from fastapi import UploadFile, File
-from test import prediction1
+from prediction import prediction1
 import uvicorn
 
 app = FastAPI()
 
+# import models
 model = tf.keras.models.load_model(os.path.abspath('model3test'))
 
 
 @app.post('/api/upload')
+# Request upload file returns predictions
 async def UploadImage(file: bytes = File(...)):
     with open('pictures/prediction/image.jpg', 'wb') as image:
         image.write(file)
         image.close()
     prediction = prediction1(model)
     return prediction
-
-
-
-
-
--- prediction.py
+++ prediction.py
@@ -5,14 +5,17 @@ from keras.preprocessing.image import ImageDataGenerator
 #from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.python.keras.models import load_model
 
+# import tensorflow.keras
 model = tf.keras.models.load_model(os.path.abspath('model3test'))
 
 
 def prediction1(model):
+    """Predictions for training"""
     test_path = os.path.abspath('pictures')
 
     BATCH_SIZE = 10
 
+    # Image preprocessing
     test_batches = ImageDataGenerator(
         preprocessing_function=tf.keras.applications.vgg16.preprocess_input, rescale=1/255.
     ).flow_from_directory(
@@ -24,9 +27,9 @@ def prediction1(model):
         color_mode='rgb',
         shuffle=False
     )
-
+    # prediction test basche
     prediction = model.predict(test_batches)
-
-    classes = ['Apple_Bad', 'Apple_Good', 'Banana_Bad', 'Banana_Good', 'Lemon_Bad', 'Lemon_Good', 'Orange_Bad', 'Orange_Good']
-    print('test')
-    return classes[np.argmax(prediction[1])]
+    # Classes should convert tensors to string
+    classes = ['Apple_Bad', 'Apple_Good', 'Banana_Bad', 'Banana_Good',
+               'Lemon_Bad', 'Lemon_Good', 'Orange_Bad', 'Orange_Good']
+    return classes[np.argmax(prediction[0])]
